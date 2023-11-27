1965d3d533ae0acc02e34145d115a0e709200028
Armando-T-T <torrestrevinoarmando@gmail.com>
2023-11-26 18:06:01 -0600

    Registro FUNCIONAL

--- ProyectoWEB/ProyectoWEB/views.py
+++ ProyectoWEB/ProyectoWEB/views.py
@@ -1,20 +1,21 @@
-"""
-Routes and views for the flask application.
-"""
-
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from ProyectoWEB import app
-from flask import Flask
-from flask import render_template, request
-from flaskext.mysql import MySQL
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/sistema'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Usuario(db.Model):
+    id_Usuario = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(255))
+    correo = db.Column(db.String(255), unique=True)
+    contrasena = db.Column(db.String(255))
 
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
     return render_template(
         'index.html',
         title='Bienvenido',
@@ -23,7 +24,6 @@ def home():
 
 @app.route('/contact')
 def contact():
-    """Renders the contact page."""
     return render_template(
         'contact.html',
         title='Contact',
@@ -33,7 +33,6 @@ def contact():
 
 @app.route('/about')
 def about():
-    """Renders the about page."""
     return render_template(
         'about.html',
         title='About',
@@ -41,12 +40,35 @@ def about():
         message='Your application description page.'
     )
 
-
-@app.route('/inicioSesion')
+@app.route('/inicioSesion', methods=['GET', 'POST'])
 def inicioSesion():
-    """Renders the inicioS page."""
-    return render_template(
-        'inicioSesion.html',
-        title='Inicia Sesion',
-        year=datetime.now().year,
-    )
+    if request.method == 'POST':
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+
+        usuario = Usuario.query.filter_by(correo=correo, contrasena=contrasena).first()
+
+        if usuario:
+            return redirect(url_for('home'))
+        else:
+            return render_template('inicioSesion.html', title='Inicia Sesion', year=datetime.now().year)
+    return render_template('inicioSesion.html', title='Inicia Sesion', year=datetime.now().year)
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    try:
+        if request.method == 'POST':
+            nombre = request.form['nombre']
+            correo = request.form['correo']
+            contrasena = request.form['contrasena']  # Asegúrate de que el nombre de la columna sea correcto
+
+            nuevo_usuario = Usuario(Nombre=nombre, correo=correo, contrasena=contrasena)
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+
+            return redirect(url_for('home'))
+
+        return render_template('registro.html', title='Regístrate', year=datetime.now().year)
+
+    except Exception as e:
+        return f"Error en el servidor: {str(e)}", 500  # Devuelve un código de estado 500 (Internal Server Error)
